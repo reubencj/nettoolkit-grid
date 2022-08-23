ec8e30804b76f695ed72cca136b4556803514d08
Reuben John <reuben.chacko.john@gmail.com>
2022-08-23 11:03:31 -0700

    made corrections based on prompt

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//get all the boxes in the grid
-const grid_boxes = document.querySelectorAll(".box");
-
 //function to create random colors using rgb values
 function random_color() {
   let rgb_color = "rgb(";
@@ -14,23 +11,32 @@ function random_color() {
   }
   return rgb_color;
 }
-let prev_box;
-//first selection of colors from the grid
-// let prev_box = Math.floor(Math.random() * 4);
-// grid_boxes[prev_box].style.backgroundColor = random_color();
+
+//get all the boxes in the grid
+const grid_boxes = document.querySelectorAll(".box");
+
+let box_time = [];
+console.log(grid_boxes);
+
+for (let i = 0; i < grid_boxes.length; i++) {
+  box_time.push({ number: i + 1, box: grid_boxes[i], time: null });
+}
+
+console.log(box_time);
 
 function change_random_box_color() {
-  let random_box = Math.floor(Math.random() * 4);
+  let exit_function = false;
+  while (exit_function === false) {
+    let random_box = Math.floor(Math.random() * 8);
 
-  //if the previous box is the same recursively call the function
-  if (prev_box == random_box) {
-    change_random_box_color();
-  } else {
-    if (prev_box >= 0) {
-      grid_boxes[prev_box].style.backgroundColor = "white";
+    const selected_box = box_time[random_box];
+
+    if (selected_box.time === null || selected_box.time <= Date.now() - 2000) {
+      selected_box.box.style.backgroundColor = random_color();
+      selected_box.time = Date.now();
+      exit_function = true;
+      console.log(selected_box);
     }
-    grid_boxes[random_box].style.backgroundColor = random_color();
-    prev_box = random_box;
   }
 }
 
